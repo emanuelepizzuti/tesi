686b6354e5c81bca34fd7ba6cd3a7e3c6dca22ff
Emanuele Pizzuti <emanuele.pizzuti96@gmail.com>
2022-03-23 12:20:59 +0100

    update grosso

--- home/script.js
+++ home/script.js
@@ -1,18 +1,68 @@
-// gsap.registerPlugin(ScrollTrigger);
-//
-// ScrollTrigger.defaults({
-//   scroller: ".container"
+// $("#menuToggle input").click(function() {
+//   $("body").toggleClass("fixed-position");
 // });
-//
-// gsap.to("#text2", {
-//   scrollTrigger: "#text2",
-//   duration: 1
-// });
-//
-// gsap.to("#text3", {
-//   scrollTrigger: "#text3",
-//   duration: 1
-// });
-$("#menuToggle input").click(function() {
-  $("body").toggleClass("fixed-position");
+
+// PAGINA 1
+window.addEventListener('scroll', function() {
+  var element = document.querySelector('#text1');
+  var position = element.getBoundingClientRect();
+
+  // checking whether fully visible
+  if (position.top >= 0 && position.bottom <= window.innerHeight) {
+    console.log('text1 is fully visible in screen');
+    $(".start").addClass("remove");
+  }
+  // checking for partial visibility
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    $(".start").addClass("remove");
+  }
+});
+// PAGINA 2
+window.addEventListener('scroll', function() {
+  var element = document.querySelector('#text2');
+  var position = element.getBoundingClientRect();
+
+  // checking whether fully visible
+  if (position.top >= 0 && position.bottom <= window.innerHeight) {
+    console.log('text2 is fully visible in screen');
+    $(".start").addClass("remove");
+  }
+  // checking for partial visibility
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    $(".start").addClass("remove");
+  }
+});
+// PAGINA 3
+window.addEventListener('scroll', function() {
+  var element = document.querySelector('#text3');
+  var position = element.getBoundingClientRect();
+
+  // checking whether fully visible
+  if (position.top >= 0 && position.bottom <= window.innerHeight) {
+    console.log('text3 is fully visible in screen');
+    $(".start").addClass("remove");
+    $(".scrolldown").removeClass("remove");
+  }
+  // checking for partial visibility
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    $(".start").addClass("remove");
+    $(".scrolldown").removeClass("remove");
+  }
+});
+// PAGINA 4
+window.addEventListener('scroll', function() {
+  var element = document.querySelector('#text4');
+  var position = element.getBoundingClientRect();
+
+  // checking whether fully visible
+  if (position.top >= 0 && position.bottom <= window.innerHeight) {
+    console.log('text4 is fully visible in screen');
+    $(".scrolldown").addClass("remove");
+    $(".start").removeClass("remove");
+  }
+  // checking for partial visibility
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    $(".scrolldown").addClass("remove");
+    $(".start").removeClass("remove");
+  }
 });
